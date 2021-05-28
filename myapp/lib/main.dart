74f9719b1dbb04c44f0ee6a28cecab2516aa7cb9
Majd Yaqub <majdya@bitbucket.com>
2021-05-28 15:09:52 +0300

    padding and marging container

--- myapp/lib/main.dart
+++ myapp/lib/main.dart
@@ -11,30 +11,38 @@ class Home extends StatelessWidget {
         centerTitle: true,
         backgroundColor: Colors.red[600],
       ),
-      body: Center(
-        // ----- Text -----
-        // child: Text(
-        //   'Hey ninjas!!',
-        //   style: TextStyle(
-        //       fontSize: 20,
-        //       fontWeight: FontWeight.bold,
-        //       letterSpacing: 2,
-        //       color: Colors.grey[600],
-        //       fontFamily: 'IndieFlower'),
-        // ),
-        // ----- Image -----
-        // child: Image.asset('assets/yuda.jpg'),
-        // ----- Icon -----
-        child: TextButton(
-          onPressed: () {},
-          child: Text(
-            'click',
-            style: TextStyle(color: Colors.black),
-          ),
-          style: TextButton.styleFrom(
-            backgroundColor: Colors.lightBlue,
-          ),
-        ),
+      // ----- Center -----
+      // body: Center(
+      // ----- Text -----
+      // child: Text(
+      //   'Hey ninjas!!',
+      //   style: TextStyle(
+      //       fontSize: 20,
+      //       fontWeight: FontWeight.bold,
+      //       letterSpacing: 2,
+      //       color: Colors.grey[600],
+      //       fontFamily: 'IndieFlower'),
+      // ),
+      // ----- Image -----
+      // child: Image.asset('assets/yuda.jpg'),
+      // ----- Button -----
+      // child: TextButton(
+      //   onPressed: () {},
+      //   child: Text(
+      //     'click',
+      //     style: TextStyle(color: Colors.black),
+      //   ),
+      //   style: TextButton.styleFrom(
+      //     backgroundColor: Colors.lightBlue,
+      //   ),
+      // ),
+      // ),
+      // ----- Container -----
+      body: Container(
+        padding: EdgeInsets.fromLTRB(10, 20, 30, 40),
+        margin: EdgeInsets.fromLTRB(20, 20, 20, 20),
+        color: Colors.grey[400],
+        child: Text('Hello'),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () => {},
