cccad97aadc0f16189ed8ab229e522037b00c6ee
Majd Yaqub <majdya@bitbucket.com>
2021-05-28 13:10:54 +0300

    styling, backgraound and fonts

--- myapp/lib/main.dart
+++ myapp/lib/main.dart
@@ -5,13 +5,22 @@ void main() => runApp(MaterialApp(
         appBar: AppBar(
           title: Text('Myfirst app'),
           centerTitle: true,
+          backgroundColor: Colors.red[600],
         ),
         body: Center(
-          child: Text('Hey ninjas!'),
+          child: Text(
+            'Hey ninjas!',
+            style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+                letterSpacing: 2,
+                color: Colors.grey[600]),
+          ),
         ),
         floatingActionButton: FloatingActionButton(
           onPressed: () => {},
           child: Text('click'),
+          backgroundColor: Colors.red[600],
         ),
       ),
     ));
