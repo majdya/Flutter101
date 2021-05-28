b27535f835f2df5f3d7ceebbbee074c1046ff7b8
Majd Yaqub <majdya@bitbucket.com>
2021-05-28 13:23:31 +0300

    enable hot reload

--- myapp/lib/main.dart
+++ myapp/lib/main.dart
@@ -1,27 +1,32 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Myfirst app'),
-          centerTitle: true,
-          backgroundColor: Colors.red[600],
-        ),
-        body: Center(
-          child: Text(
-            'Hey ninjas!',
-            style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
-                letterSpacing: 2,
-                color: Colors.grey[600],
-                fontFamily: 'IndieFlower'),
-          ),
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () => {},
-          child: Text('click'),
-          backgroundColor: Colors.red[600],
+void main() => runApp(MaterialApp(home: Home()));
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Myfirst app'),
+        centerTitle: true,
+        backgroundColor: Colors.red[600],
+      ),
+      body: Center(
+        child: Text(
+          'Hey ninjas!!',
+          style: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+              letterSpacing: 2,
+              color: Colors.grey[600],
+              fontFamily: 'IndieFlower'),
         ),
       ),
-    ));
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => {},
+        child: Text('click'),
+        backgroundColor: Colors.red[600],
+      ),
+    );
+  }
+}
