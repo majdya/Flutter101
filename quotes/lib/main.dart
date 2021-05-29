f60da25d2ad194b6b85e4a6cbb077654f65b0624
majdyaq <majd@feelit.tech>
2021-05-30 01:30:02 +0300

    Card template function

--- quotes/lib/main.dart
+++ quotes/lib/main.dart
@@ -14,6 +14,30 @@ class _QuoteListState extends State<QuoteList> {
     Quote(author: 'author2', text: "text 2"),
     Quote(author: 'author3', text: "text 3"),
   ];
+
+  Widget quoteTemplate(quote) {
+    return Card(
+      margin: EdgeInsets.fromLTRB(16, 16, 16, 0),
+      child: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Text(quote.text,
+                style: TextStyle(fontSize: 18, color: Colors.grey[600])),
+            SizedBox(
+              height: 6,
+            ),
+            Text(
+              quote.author,
+              style: TextStyle(color: Colors.grey[600], fontSize: 14),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -24,10 +48,7 @@ class _QuoteListState extends State<QuoteList> {
         backgroundColor: Colors.redAccent,
       ),
       body: Column(
-        children: quotes
-            .map(
-                (quote) => Text('text: ${quote.text}, author: ${quote.author}'))
-            .toList(),
+        children: quotes.map((quote) => quoteTemplate(quote)).toList(),
       ),
     );
   }
