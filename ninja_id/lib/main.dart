b7b843e67be9b78830a8e1cbe13a10c679d096b7
majdyaq <majd@feelit.tech>
2021-05-29 19:33:39 +0300

    static ID card complete

--- ninja_id/lib/main.dart
+++ ninja_id/lib/main.dart
@@ -8,8 +8,72 @@ class NinjaCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey[900],
       appBar: AppBar(
         title: Text('Ninja ID Card'),
+        centerTitle: true,
+        backgroundColor: Colors.grey[850],
+        elevation: 0,
+      ),
+      body: Padding(
+        padding: EdgeInsets.fromLTRB(30, 40, 30, 0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Center(
+              child: CircleAvatar(
+                backgroundImage: AssetImage('assets/yuda.jpg'),
+                radius: 40,
+              ),
+            ),
+            Divider(
+              height: 70,
+              color: Colors.grey[600],
+            ),
+            Text(
+              'NAME',
+              style: TextStyle(color: Colors.grey, letterSpacing: 2),
+            ),
+            SizedBox(height: 10),
+            Text(
+              'Majd',
+              style: TextStyle(
+                  color: Colors.amber[200],
+                  fontSize: 28,
+                  fontWeight: FontWeight.bold,
+                  letterSpacing: 2),
+            ),
+            SizedBox(height: 30),
+            Text(
+              'CURRENT NINJA LEVEL',
+              style: TextStyle(color: Colors.grey, letterSpacing: 2),
+            ),
+            SizedBox(height: 10),
+            Text(
+              '10',
+              style: TextStyle(
+                  color: Colors.amber[200],
+                  fontSize: 28,
+                  fontWeight: FontWeight.bold,
+                  letterSpacing: 2),
+            ),
+            SizedBox(height: 30),
+            Row(
+              children: <Widget>[
+                Icon(Icons.email, color: Colors.grey[400]),
+                SizedBox(width: 10),
+                Text(
+                  'majd.ya@dev.com',
+                  style: TextStyle(
+                    color: Colors.grey[400],
+                    fontSize: 18,
+                    letterSpacing: 1,
+                  ),
+                )
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
